31ca6fb9563b107ff971160ce1d450674cac13f2
Firenze Higa Putra <firenzehiga@gmail.com>
2025-09-28 19:10:04 +0700

    refactor: menggunakan map untuk looping productList

--- src/utils/image.js
+++ src/utils/image.js
@@ -1,5 +1,4 @@
 import atomic from "@/books/atomic.jpg";
-import img72 from "@/books/img-72.jpg";
 import img1 from "@/books/1.png";
 import img2 from "@/books/2.png";
 import img3 from "@/books/3.png";
@@ -9,25 +8,81 @@ import img6 from "@/books/6.png";
 import img7 from "@/books/7.png";
 import img8 from "@/books/8.png";
 import img9 from "@/books/9.png";
-import img10 from "@/books/10.png";
-import img11 from "@/books/11.png";
-import img12 from "@/books/12.png";
-import img13 from "@/books/13.png";
 
-export {
-  atomic,
-  img72,
-  img1,
-  img2,
-  img3,
-  img4,
-  img5,
-  img6,
-  img7,
-  img8,
-  img9,
-  img10,
-  img11,
-  img12,
-  img13,
-};
+export { atomic };
+
+// Product data for mapping in Product List
+export const products = [
+	{
+		id: 1,
+		img: img1,
+		title: "The First Book",
+		description:
+			"National Book Award-finalist Ibi Zoboi makes her debut with an unforgettable character.",
+		date: "August 27, 2019",
+	},
+	{
+		id: 2,
+		img: img2,
+		title: "Bakary's Adventure",
+		description:
+			"Meet Bakary, a curious and adventurous African boy that loves animals.",
+		date: "November 2017",
+	},
+	{
+		id: 3,
+		img: img3,
+		title: "Fundraising Secrets",
+		description:
+			"Drawing on knowledge gained from her past career as a fundraising consultant.",
+		date: "August 20, 2019",
+	},
+	{
+		id: 4,
+		img: img4,
+		title: "Klingon & Hobbit",
+		description:
+			"Darius Kellner speaks better Klingon than Farsi, and he knows more about Hobbit social cues.",
+		date: "August 28, 2018",
+	},
+	{
+		id: 5,
+		img: img5,
+		title: "Underworld Stories",
+		description:
+			"In the history of New York City’s underworld, there have been a host of criminal figures.",
+		date: "August 27, 2019",
+	},
+	{
+		id: 6,
+		img: img6,
+		title: "Aja James",
+		description:
+			"Freshly forty-five, Aja James knows that her life is good, complete with a loving.",
+		date: "August 27, 2019",
+	},
+	{
+		id: 7,
+		img: img7,
+		title: "Hard-won Wisdom",
+		description:
+			"Rich with hard-won wisdom and humanity, set in locales from Miami and Port-au-Prince.",
+		date: "August 27, 2019",
+	},
+	{
+		id: 8,
+		img: img8,
+		title: "Wildly Spanning",
+		description:
+			"Among its residents—wildly spanning decades, perspectives, and species—are David Sherman.",
+		date: "August 20, 2019",
+	},
+	{
+		id: 9,
+		img: img9,
+		title: "Melody's Coming of Age",
+		description:
+			"As the book opens, it is the evening of sixteen-year-old Melody's coming of ceremony.",
+		date: "September 17, 2019",
+	},
+];
