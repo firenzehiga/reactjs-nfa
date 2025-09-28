8cca37d411b187fe7623aa312808c2bf5fd9a53c
Firenze Higa Putra <firenzehiga@gmail.com>
2025-09-28 19:20:26 +0700

    feat: upd

--- src/utils/image.js
+++ src/utils/image.js
@@ -11,12 +11,12 @@ import img9 from "@/books/9.png";
 
 export { atomic };
 
-// Product data for mapping in Product List
+// Buat array products
 export const products = [
 	{
 		id: 1,
 		img: img1,
-		title: "The First Book",
+		title: "My Life as an Ice Cream Sandwich",
 		description:
 			"National Book Award-finalist Ibi Zoboi makes her debut with an unforgettable character.",
 		date: "August 27, 2019",
@@ -24,7 +24,7 @@ export const products = [
 	{
 		id: 2,
 		img: img2,
-		title: "Bakary's Adventure",
+		title: "Bakary On Safari",
 		description:
 			"Meet Bakary, a curious and adventurous African boy that loves animals.",
 		date: "November 2017",
@@ -32,7 +32,7 @@ export const products = [
 	{
 		id: 3,
 		img: img3,
-		title: "Fundraising Secrets",
+		title: "The Memo",
 		description:
 			"Drawing on knowledge gained from her past career as a fundraising consultant.",
 		date: "August 20, 2019",
@@ -40,7 +40,7 @@ export const products = [
 	{
 		id: 4,
 		img: img4,
-		title: "Klingon & Hobbit",
+		title: "Darius the Great is Not Okay",
 		description:
 			"Darius Kellner speaks better Klingon than Farsi, and he knows more about Hobbit social cues.",
 		date: "August 28, 2018",
@@ -48,7 +48,7 @@ export const products = [
 	{
 		id: 5,
 		img: img5,
-		title: "Underworld Stories",
+		title: "Wrath K'wan",
 		description:
 			"In the history of New York City’s underworld, there have been a host of criminal figures.",
 		date: "August 27, 2019",
@@ -56,7 +56,7 @@ export const products = [
 	{
 		id: 6,
 		img: img6,
-		title: "Aja James",
+		title: "More to Life",
 		description:
 			"Freshly forty-five, Aja James knows that her life is good, complete with a loving.",
 		date: "August 27, 2019",
@@ -64,7 +64,7 @@ export const products = [
 	{
 		id: 7,
 		img: img7,
-		title: "Hard-won Wisdom",
+		title: "Everthing Inside",
 		description:
 			"Rich with hard-won wisdom and humanity, set in locales from Miami and Port-au-Prince.",
 		date: "August 27, 2019",
@@ -72,7 +72,7 @@ export const products = [
 	{
 		id: 8,
 		img: img8,
-		title: "Wildly Spanning",
+		title: "The World Doesn't Require You",
 		description:
 			"Among its residents—wildly spanning decades, perspectives, and species—are David Sherman.",
 		date: "August 20, 2019",
@@ -80,7 +80,7 @@ export const products = [
 	{
 		id: 9,
 		img: img9,
-		title: "Melody's Coming of Age",
+		title: "Red At The Bone",
 		description:
 			"As the book opens, it is the evening of sixteen-year-old Melody's coming of ceremony.",
 		date: "September 17, 2019",
